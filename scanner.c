3a69a11af73ac0d9618dd6c955b2bc495df357bc
akvitkin <71415420+akvitkin@users.noreply.github.com>
2021-09-02 15:26:53 -0300

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -2,36 +2,40 @@
 #include <ctype.h>
 #include "scanner.h"
 
-void get_token(char c, int state) {
+
+void validacion_state(char c, int state){
   if (!state) {
     if (c == EOF) {
       get_token(c, FDT);
     } else if (c == ',') {
-      get_token(c, SEP);
+      get_token(c, SEPARADOR);
     } else if (!isspace(c)) {
-      get_token(c, CAD);
+      get_token(c, CADENA);
     }
-    return;
   }
+}
 
-  if (state == CAD) {
+void imprimir_cadena(char c, int state){
+  if (state == CADENA) {
     char current;
     printf("Cadena: ");
     putchar(c);
+
     while ((current = getchar()) != EOF && !isspace(current) && current != ',') {
       putchar(current);
     }
-
     if (current == ',') {
       printf("\n");
-      get_token(current, SEP);
+      get_token(current, SEPARADOR);
       return;
     }
-
     printf("\n");
   }
+}
 
-  if (state == SEP) {
+
+void imprimir_separador_fdt(int state){
+    if (state == SEPARADOR) {
     printf("Separador: ,\n");
   }
 
@@ -39,3 +43,17 @@ void get_token(char c, int state) {
     printf("Fin De Texto: \n");
   }
 }
+
+
+void get_token(char c, int state) {
+
+  validacion_state(c, state);
+
+  imprimir_cadena(c, state);
+
+  imprimir_separador_fdt(state);
+
+}
+
+
+
